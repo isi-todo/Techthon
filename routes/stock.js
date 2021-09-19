b7d356423fdb2fb6b7534aae1a0ff97823a1a7e8
isi-todo <todo@isi-grp.co.jp>
2021-09-19 22:37:01 +0900

    Mission #3

--- routes/stock.js
+++ routes/stock.js
@@ -68,5 +68,123 @@ router.get('/detail/:id?', async (req, res, next) => {
   }
 });
 
+/* 商品在庫一覧 */
+router.get('/list', async (req, res, next) => {
+  let conn = await mysql.createConnection(dbConfig);
+  try {
+    let sql = 'select id, name, price, on_sale, count from stock';
+    let condition = '';
+    let sort = ' order by id asc';
+    let param = [];
+
+    // 最小値
+    if (req.query.min_count) {
+      condition ? condition += ' and ' : condition += ' where ';
+      condition += 'count >= ?';
+      param.push(Number(req.query.min_count));
+    }
+
+    // 最大値
+    if (req.query.max_count) {
+      condition ? condition += ' and ' : condition += ' where ';
+      condition += 'count <= ?';
+      param.push(Number(req.query.max_count));
+    }
+
+    // 販売中
+    if (req.query.on_sale) {
+      condition ? condition += ' and ' : condition += ' where ';
+      condition += 'on_sale = ?';
+      param.push(req.query.on_sale == 'true' ? true : false);
+    }
+
+    const [rows1, fields1] = await conn.execute(sql + condition + sort, param);
+
+    console.log('select completed');
+
+    res.json({
+      status_code: 200,
+      method: 'GET',
+      items: rows1
+    });
+
+  } catch (e) {
+    console.log(e)
+    res.json({
+      status_code: 400,
+      method: 'GET'
+    });  
+  } finally {
+    conn.end();
+  }
+});
+
+/* 商品在庫更新 */
+router.put('/update/:id', async (req, res, next) => {
+  let conn = await mysql.createConnection(dbConfig);
+  try {
+    await conn.beginTransaction();
+
+    const [rows1, fields1] = await conn.query('update stock set ? where id = ?', [req.body, req.params.id]);
+
+    await conn.commit();
+    console.log('update completed');
+
+    if(rows1.affectedRows > 0) {
+      res.json({
+        status_code: 200,
+        method: 'PUT'
+      });
+    } else {
+      res.json({
+        status_code: 404,
+        method: 'PUT'
+      });
+    }
+  } catch (e) {
+    await conn.rollback();
+    console.log(e)
+    res.json({
+      status_code: 400,
+      method: 'PUT'
+    })
+  } finally {
+    conn.end();
+  }
+});
+
+/* 商品在庫削除 */
+router.delete('/delete/:id', async (req, res, next) => {
+  let conn = await mysql.createConnection(dbConfig);
+  try {
+    await conn.beginTransaction();
+
+    const [rows1, fields1] = await conn.query('delete from stock where id = ?', req.params.id);
+
+    await conn.commit();
+    console.log('delete completed');
+
+    if(rows1.affectedRows > 0) {
+      res.json({
+        status_code: 200,
+        method: 'DELETE'
+      });
+    } else {
+      res.json({
+        status_code: 404,
+        method: 'DELETE'
+      });
+    }
+  } catch (e) {
+    await conn.rollback();
+    console.log(e)
+    res.json({
+      status_code: 400,
+      method: 'DELETE'
+    })
+  } finally {
+    conn.end();
+  }
+});
 
 module.exports = router;
